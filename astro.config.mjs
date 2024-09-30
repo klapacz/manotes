994e99066d1c971e25d9fea16e962f90628e9c6d
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2024-09-30 23:02:25 +0200

    chore: add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,22 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
+
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: "server",
+
   adapter: cloudflare({
     platformProxy: {
-      enabled: true
-    }
-  })
+      enabled: true,
+    },
+  }),
+
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), react()],
 });
