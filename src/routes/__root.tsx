31327b83427ef6c005cb30a2ff2a97d661e9b8f9
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2025-11-30 22:11:22 +0100

    init

---
+++ src/routes/__root.tsx
@@ -0,0 +1,29 @@
+import {
+  HeadContent,
+  Outlet,
+  Scripts,
+  createRootRouteWithContext,
+} from '@tanstack/solid-router'
+import { TanStackRouterDevtools } from '@tanstack/solid-router-devtools'
+
+import styleCss from '../styles.css?url'
+
+export const Route = createRootRouteWithContext()({
+  head: () => ({
+    links: [{ rel: 'stylesheet', href: styleCss }],
+  }),
+  shellComponent: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <>
+      <HeadContent />
+
+      <Outlet />
+      <TanStackRouterDevtools />
+
+      <Scripts />
+    </>
+  )
+}
