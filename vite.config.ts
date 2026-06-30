0e51305344da9624f13762ee0779446aa961bf19
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2026-06-30 22:45:06 +0200

    chore: vite ignore .workspace/

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import { defineConfig } from "vite-plus";
 export default defineConfig({
   // oxfmt doesn't support nested configs yet
   fmt: {
-    ignorePatterns: ["apps/web/drizzle/meta/**", "apps/worker/worker-configuration.d.ts"],
+    ignorePatterns: [
+      "apps/web/drizzle/meta/**",
+      "apps/worker/worker-configuration.d.ts",
+      ".workspace/**",
+    ],
   },
-  lint: { options: { typeAware: true, typeCheck: true } },
+  lint: {
+    ignorePatterns: [".workspace/**"],
+    options: { typeAware: true, typeCheck: true },
+  },
   test: {
-    exclude: ["**/node_modules/**", ".reference/**", ".opencode/**", ".direnv"],
+    exclude: ["**/node_modules/**", ".reference/**", ".opencode/**", ".direnv", ".workspace/**"],
   },
 });
